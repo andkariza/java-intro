68c66e43dba2a784c194c93dcf3f98427d8208d3
stabelg <andressakariza@gmail.com>
2023-04-02 12:23:17 +0300

    Function - example

---
+++ src/main/java/org/example/FunctionExample.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class FunctionExample {
+    public static void main(String[] args) {
+        String words = "Hi, Welcome to intro to programming!";
+        System.out.println("Words before reversing the order of letters: " + words);
+        // 1). How to create StringBuilder based n String
+        StringBuilder reverseHelper = new StringBuilder(words);
+
+        // 2). How to use reverse operation
+        reverseHelper.reverse();
+
+        String revertedWords = reverseHelper.toString();
+        System.out.println(revertedWords);
+
+    }
+}
